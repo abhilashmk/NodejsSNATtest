cde06d24ee1e2c025e998229c846cdde61eb390e
Abhilash Konnur <abkonnur@microsoft.com>
2020-10-29 21:32:30 +0530

    latest commit

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const https = require('https');
 const HttpsAgent = require('agentkeepalive').HttpsAgent;
 
 const keepaliveAgent = new HttpsAgent({
-    maxSockets: 100
+    maxSockets: 100,
+    maxFreeSockets: 10
   });
 //const superagent = require('superagent');
 var app = express();
 
 const options = {
-    host: 'www.npmjs.com',
+    host: 'www.google.com',
     port: 443,
-    path: '/package/agentkeepalive',
+    path: '/search?q=nodejs&sugexp=chrome,mod=12&sourceid=chrome&ie=UTF-8',
     method: 'GET',
     agent: keepaliveAgent,
   };
@@ -21,15 +22,19 @@ app.get('/', function (req, res) {
  // res.send('Hello abhilash branch World!');
 //var response=superagent.agent(keepaliveAgent).get('https://google.com');
 
-    const req1 = https.request(options, res1 => {
+        const req1 = https.request(options, res1 => {
         console.log('STATUS: ' + res1.statusCode);
-      });
-      req1.on('error', e => {
-        console.log('problem with request: ' + e.message);
-      });
-      req1.end();
-
-console.log(keepaliveAgent.getCurrentStatus());
+        req1.end();
+        });
+        req1.on('error', e => {
+            console.log('problem with request: ' + e.message);
+          });
+        req1.end();
+    
+      
+        setTimeout(() => {
+            console.log('agent status: %j', keepaliveAgent.getCurrentStatus());
+          }, 2000);
 
 res.send("SNAT keepalive agent test with SUperAgent HTTP client");
  
